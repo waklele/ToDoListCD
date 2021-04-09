37bd27d8a7e4ce46773deab0c7e2a54c4ad54078
waklele <complexrangga@gmail.com>
2021-04-09 15:18:17 +0700

    Initial Commit

---
+++ ToDoListCD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListCD
+//
+//  Created by Rangga Rentya on 09/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
